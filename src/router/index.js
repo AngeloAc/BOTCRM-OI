341a7d94e269e60365659ddcfe5aac7e08ca9f5c
AngeloAc <euclidescarvalho1904@gmail.com>
2023-06-27 00:38:30 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// const route = router.get('/', (req, res, next) => {
-//     res.status(200).send({
-//         title: "API CODE",
-//         version: "0.0.01",
-//         method: "GET"
-//     })
-// });
-
-router.get('/', (req, res, next) => {
+const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "API CODE",
         version: "0.0.01",
@@ -17,17 +9,5 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/myuser', (req, res, next) => {
-    
-    const name = req.body.name;
-    const phone = req.body.phone;
-    console.log("name: " + name + "phone: " + phone);
-    res.status(200).send({
-        name: name,
-        phone: phone
-    })
-});
 
-// module.exports = route;
-
-module.exports = router;
+module.exports = route;
