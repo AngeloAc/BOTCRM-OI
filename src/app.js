e8b3e1403b76c076d14a50800bdb8bedc95affff
AngeloAc <euclidescarvalho1904@gmail.com>
2023-06-27 09:53:15 +0100

    update app

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const index = require('./router/index');
 const user = require('./router/user');
 const whatsapp = require('./router/whatsapp');
 
-// conexao com banco de dado...
+// conexao com banco de dado upt...
 // mongo.connect(process.env.MONGO_CONNECT_URI).
 const MONGO_CONNECT_URI = "mongodb+srv://euclidescarvalho:QsZPmnNtPUY6KLZA@ipsdata.hp31cyr.mongodb.net/?retryWrites=true&w=majority";
 mongo.connect(process.env.MONGO_CONNECT_URI).
