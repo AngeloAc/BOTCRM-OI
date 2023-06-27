7b25df74edea9512064791e62633c921c4dd48a5
AngeloAc <euclidescarvalho1904@gmail.com>
2023-06-27 09:57:22 +0100

    .env is comment

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const whatsapp = require('./router/whatsapp');
 // conexao com banco de dado upt...
 // mongo.connect(process.env.MONGO_CONNECT_URI).
 const MONGO_CONNECT_URI = "mongodb+srv://euclidescarvalho:QsZPmnNtPUY6KLZA@ipsdata.hp31cyr.mongodb.net/?retryWrites=true&w=majority";
-mongo.connect(process.env.MONGO_CONNECT_URI).
+mongo.connect(MONGO_CONNECT_URI).
     then(() => console.log("Connected to db")).catch(error => console.log("Ocorreu um erro ao criar o banco de dados!" + error.message));
 
 app.use(body_parser.json());
