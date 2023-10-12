ec40b83eea9bda495a545030a5027c2dffcef25e
AngeloAc <euclidescarvalho1904@gmail.com>
2023-10-12 14:36:35 +0100

    update

--- src/app.js
+++ src/app.js
@@ -16,16 +16,16 @@ mongo.connect(process.env.MONGO_CONNECT_URI).
     then(() => console.log("> A sua frontend está agora conectado ao seu banco de dados.")).catch(error => console.log("> Ocorreu um erro ao criar o banco de dados!" + error.message));
 
     // Configurar Content Security Policy
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://chat.startic.ao"); // Substitua com seu domínio
-    // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// app.use(cors())
-    return next();
-});
-// app.use(cors({
-//     origin: '*',
-// }))
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://chat.startic.ao"); // Substitua com seu domínio
+//     // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// // app.use(cors())
+//     return next();
+// });
+app.use(cors({
+    origin: '*',
+}))
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
